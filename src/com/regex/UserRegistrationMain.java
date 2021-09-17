b16d3db1a98704f55024e6d2e6f4aafb3091d6b3
sreeliptaa <sreelipta65@gmail.com>
2021-09-17 15:02:44 +0530

    Added: welcome message

---
+++ src/com/regex/UserRegistrationMain.java
@@ -0,0 +1,9 @@
+package com.regex;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        //Welcome Message
+        System.out.println("Welcome to the User Registration and Validation Program");
+    }
+
+}
